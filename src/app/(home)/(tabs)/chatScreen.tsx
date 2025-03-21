7a4d49ea0e9aec16521bbdbb459a3140a7b8f247
Jerry Wang <79975526+thehoodhobo@users.noreply.github.com>
2025-03-21 05:03:41 -0400

    streamchat setup complete, need to implment chat avatar and discovery screen

--- src/app/(home)/(tabs)/chatScreen.tsx
+++ src/app/(home)/(tabs)/chatScreen.tsx
@@ -3,12 +3,15 @@ import {useState} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Channel as ChannelType } from "stream-chat";
 import { ChannelList } from 'stream-chat-expo';
+import { useAuth } from '../../../providers/AuthProvider';
 
 const ChatScreen = () => {
     const [channel, setChannel] = useState<ChannelType|null>();
+    const {user} = useAuth();
     return (
         
         <ChannelList 
+            filters={{members: { $in: user?.id ? [user.id] : [] }}}
             onSelect={(channel) => router.push(`/channel/${channel?.cid}`)}/>
     );
 };
--- src/app/(home)/(tabs)/profileScreen.tsx
+++ src/app/(home)/(tabs)/profileScreen.tsx
@@ -1,118 +1,64 @@
 import { View, StyleSheet, Alert } from "react-native";
-import { useState, useEffect } from 'react'
-import { supabase } from '../../../lib/supabase'
-import { useAuth } from '../../../providers/AuthProvider'
-import { Button, Input } from '@rneui/themed'
-import Avatar from '../../../components/avatar'
+import { useEffect, useState } from "react";
+import { useAuth } from "../../../providers/AuthProvider";
+import { Button, Input } from "@rneui/themed";
+import Avatar from "../../../components/avatar";
 import { ScrollView } from "react-native-gesture-handler";
-
+import { supabase } from "../../../lib/supabase";
 
 export default function ProfileScreen() {
-  const {session} = useAuth()
-  const [loading, setLoading] = useState(true)
-  const [name, setName] = useState('')
-  const [age, setAge] = useState('')
-  const [avatarUrl, setAvatarUrl] = useState('')
+  const { session, profile, photos,  isLoading, fetchProfile, fetchProfilePhoto, } = useAuth();
+  
+  const [name, setName] = useState(profile?.name || "");
+  const [age, setAge] = useState(profile?.age?.toString() || "");
+  const [ethnicity, setEthnicity] = useState(profile?.ethnicity || "");
+  const [familyPlans, setFamilyPlans] = useState(profile?.family_plans || "");
+  const [height, setHeight] = useState(profile?.height?.toString() || "");
+  const [zodiacSign, setZodiacSign] = useState(profile?.zodiac_sign || "");
+  const [pets, setPets] = useState(profile?.pets ? "Yes" : "No");
+  const [avatarUrl, setAvatarUrl] = useState((photos?.[0] ?? "") || "");
 
   useEffect(() => {
-    if (session) getProfile()
-      console.log('looking for profile', session)
-  }, [session])
-
-  async function getProfile() {
+    fetchProfile(); 
+    fetchProfilePhoto();
+  }, []);
+  console.log("profile in profile screen is: ", avatarUrl);
+  async function updateProfile() {
     try {
-      setLoading(true)
-      if (!session?.user) throw new Error('No user on the session!')
-
-        const { data: profileData, error: profileError, status: profileStatus } = await supabase
-        .from('profiles')
-        .select(`name, age`)
-        .eq('id', session?.user.id)
-        .single();
-  
-      if (profileError && profileStatus !== 406) {
-        throw profileError;
-      }
-
-      const { data: photosData, error: photosError } = await supabase
-      .from('profile_photos')
-      .select(`file_path`)
-      .eq('user_id', session?.user.id);
-      console.log('Got photos filepath', photosData)
-
-      if (photosError) {
-        throw photosError;
-      }
-      if (profileData) {
-        console.log('Got profile data', profileData.age)
-        setName(profileData.name)
-        setAge(profileData.age.toString())
-        // setAvatarUrl(data.avatar_url)
-      }
-
-      if (photosData) {
-        const photoUrls = photosData.map((photo) => photo.file_path);
-        setAvatarUrl(photoUrls[0]); // Assuming you have a state variable `setPhotos`
-      }
-
-  
-    } catch (error) {
-      if (error instanceof Error) {
-        Alert.alert(error.message)
-      }
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  async function updateProfile({
-    name,
-    age,
-    // avatar_url,
-  }: {
-    name: string
-    age: number
-    // avatar_url: string
-  }) {
-    try {
-      setLoading(true)
-      if (!session?.user) throw new Error('No user on the session!')
+      if (!session?.user) throw new Error("No user on the session!");
 
       const updates = {
-        id: session?.user.id,
+        id: session.user.id,
         name,
-        age,
-        // avatar_url,
+        age: Number(age) || null,
+        ethnicity,
+        family_plans: familyPlans,
+        height: Number(height) || null,
+        zodiac_sign: zodiacSign,
+        pets: pets === "Yes",
         updated_at: new Date(),
-      }
+      };
 
-      const { error } = await supabase.from('profiles').upsert(updates)
+      const { error } = await supabase.from("profiles").upsert(updates);
+      if (error) throw error;
 
-      if (error) {
-        throw error
-      }
+      fetchProfile();
+      fetchProfilePhoto();
+ // Refresh context data after update
     } catch (error) {
-      if (error instanceof Error) {
-        Alert.alert(error.message)
-      }
-    } finally {
-      setLoading(false)
+      if (error instanceof Error) Alert.alert(error.message);
     }
   }
 
   return (
     <ScrollView style={styles.container}>
-      <View style={{ alignItems: 'center' }}>
+      <View style={{ alignItems: "center" }}>
         <Avatar
           size={200}
           url={avatarUrl}
           onUpload={(url: string) => {
-            setAvatarUrl(url)
-            updateProfile({ 
-              name, 
-              age: Number(age), 
-              // avatar_url: url })
-            })
+            setAvatarUrl(url);
+            updateProfile();
           }}
         />
       </View>
@@ -120,25 +66,36 @@ export default function ProfileScreen() {
         <Input label="Email" value={session?.user?.email} disabled />
       </View>
       <View style={styles.verticallySpaced}>
-        <Input label="Name" value={name || ''} onChangeText={(text) => setName(text)} />
+        <Input label="Name" value={name} onChangeText={setName} />
       </View>
       <View style={styles.verticallySpaced}>
-        <Input label="Age" value={age} onChangeText={(text) => setAge(text)} keyboardType="numeric" />
+        <Input label="Age" value={age} onChangeText={setAge} keyboardType="numeric" />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Input label="Ethnicity" value={ethnicity} onChangeText={setEthnicity} />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Input label="Family Plans" value={familyPlans} onChangeText={setFamilyPlans} />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Input label="Height (cm)" value={height} onChangeText={setHeight} keyboardType="numeric" />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Input label="Zodiac Sign" value={zodiacSign} onChangeText={setZodiacSign} />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Input label="Pets" value={pets} onChangeText={setPets} />
       </View>
 
       <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Button
-          title={loading ? 'Loading ...' : 'Update'}
-          onPress={() => updateProfile({ name, age: Number(age) })}
-          disabled={loading}
-        />
+        <Button title={isLoading ? "Loading ..." : "Update"} onPress={updateProfile} disabled={isLoading} />
       </View>
 
       <View style={styles.verticallySpaced}>
         <Button title="Sign Out" onPress={() => supabase.auth.signOut()} />
       </View>
     </ScrollView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -149,9 +106,9 @@ const styles = StyleSheet.create({
   verticallySpaced: {
     paddingTop: 4,
     paddingBottom: 4,
-    alignSelf: 'stretch',
+    alignSelf: "stretch",
   },
   mt20: {
     marginTop: 20,
   },
-})
+});
